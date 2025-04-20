935fc035f9adf4bb549443fac844a9e5e6963619
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-20 17:46:19 -0300

    feat: impl onLogin event

--- authentication-frontend/src/app/content/content.component.ts
+++ authentication-frontend/src/app/content/content.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { WelcomeContentComponent } from '../welcome-content/welcome-content.component';
+import { LoginFormComponent } from '../login-form/login-form.component';
+import { AxiosService } from '../axios.service';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [WelcomeContentComponent, LoginFormComponent],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrl: './content.component.css',
 })
 export class ContentComponent {
+  constructor(private readonly axiosService: AxiosService) {}
 
+  onLogin(input: any): void {
+    this.axiosService.request(
+      "POST",
+      "/login",
+      {
+        login: input.login,
+        password: input.password
+      }
+    );
+  }
 }

3fa10a8db3f153ec40b950c1387e7af6a8c5d759
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-20 14:46:43 -0300

    feat:  impl auth-content

--- authentication-frontend/src/app/auth-content/auth-content.component.ts
+++ authentication-frontend/src/app/auth-content/auth-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AxiosService } from '../axios.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth-content',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './auth-content.component.html',
   styleUrl: './auth-content.component.css'
 })

be6fb77705d5a0df29769bcd2cf059c33d975bd2
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-20 13:21:45 -0300

    feat: create auth-content

---
+++ authentication-frontend/src/app/auth-content/auth-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-content',
+  imports: [],
+  templateUrl: './auth-content.component.html',
+  styleUrl: './auth-content.component.css'
+})
+export class AuthContentComponent {
+
+}

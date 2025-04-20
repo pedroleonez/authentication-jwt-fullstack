ef2955738330839bbc8137a801692b6aff3cd888
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-20 17:45:37 -0300

    feat: impl login form

--- authentication-frontend/src/app/login-form/login-form.component.ts
+++ authentication-frontend/src/app/login-form/login-form.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+  @Output() onSubmitLoginEvent = new EventEmitter();
 
+  login: string =  '';
+  password: string = '';
+
+  onSubmitLogin() {
+    this.onSubmitLoginEvent.emit({'login': this.login, 'password': this.password});
+  }
 }

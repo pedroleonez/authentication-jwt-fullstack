d9086b5e85e1d3b62ea0284cfccea1d4808b177c
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-20 14:47:01 -0300

    feat: add auth-content

--- authentication-frontend/src/app/app.component.ts
+++ authentication-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { AuthContentComponent } from './auth-content/auth-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AuthContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

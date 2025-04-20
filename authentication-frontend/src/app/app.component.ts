6ad19c9ad95733178843002e338fa662ccb35f6a
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-20 17:46:57 -0300

    feat: change to show content component

--- authentication-frontend/src/app/app.component.ts
+++ authentication-frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AuthContentComponent } from './auth-content/auth-content.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AuthContentComponent],
+  imports: [RouterOutlet, HeaderComponent, AuthContentComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

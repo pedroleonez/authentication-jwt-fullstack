eda90d175fed73b46ddfd44b68c23aa2e95dfc91
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-21 12:36:51 -0300

    feat: create and impl AuthController

---
+++ authentication-backend/src/main/java/pedroleonez/authentication_backend/controllers/AuthController.java
@@ -0,0 +1,38 @@
+package pedroleonez.authentication_backend.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pedroleonez.authentication_backend.config.UserAuthenticationProvider;
+import pedroleonez.authentication_backend.dtos.CredentialsDto;
+import pedroleonez.authentication_backend.dtos.SignUpDto;
+import pedroleonez.authentication_backend.dtos.UserDto;
+import pedroleonez.authentication_backend.services.UserService;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+public class AuthController {
+
+    private final UserService userService;
+    private final UserAuthenticationProvider userAuthenticationProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDto> login(@RequestBody @Valid CredentialsDto credentialsDto) {
+        UserDto userDto = userService.login(credentialsDto);
+        userDto.setToken(userAuthenticationProvider.createToken(userDto));
+        return ResponseEntity.ok(userDto);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> register(@RequestBody @Valid SignUpDto user) {
+        UserDto createdUser = userService.register(user);
+        createdUser.setToken(userAuthenticationProvider.createToken(createdUser));
+        return ResponseEntity.created(URI.create("/users/" + createdUser.getId())).body(createdUser);
+    }
+
+}
